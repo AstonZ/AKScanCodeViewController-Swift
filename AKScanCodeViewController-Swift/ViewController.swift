e3768a8d7d5103d736fa2015cc95cc5680a7fb3e
Aston Zhang <liangziokay@gmail.com>
2015-11-18 10:04:21 +0800

    Initial Commit

---
+++ AKScanCodeViewController-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AKScanCodeViewController-Swift
+//
+//  Created by 张良 on 15/11/18.
+//  Copyright © 2015年 Aston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
